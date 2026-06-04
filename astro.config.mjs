a39899dd9e0417be51d2abfa37b6041a2735a355
Douglas Starnes <douglas.a.starnes@gmail.com>
2026-06-04 11:42:39 -0500

    Add curriculum modules for GitHub Models, Onboarding, OpenAI SDK, and Python Development
    - Created a new module for GitHub Models to explore LLM hosting options.
    - Added an onboarding module detailing the setup of GitHub Codespaces for Python and AI development.
    - Introduced a module on using the OpenAI SDK for Python to interact with models hosted on GitHub.
    - Developed a module for Python development using GitHub Codespaces and Jupyter Notebook.

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,32 +21,58 @@ export default defineConfig({
         //   slug: "workshop/module01",
         // },
         {
-          label: "Onboarding",
-          slug: "workshop/module02",
+          label: "Current Version",
+          items: [
+            {
+              label: "Onboarding",
+              slug: "workshop/module02",
+            },
+            {
+              label: "Python Development with GitHub Codespaces",
+              slug: "workshop/module03",
+            },
+            {
+              label: "GitHub Models",
+              slug: "workshop/module04",
+            },
+            {
+              label: "OpenAI SDK",
+              slug: "workshop/module05",
+            },
+            {
+              label: "Interactive Development with Jupyter Notebook",
+              slug: "workshop/module06",
+            },
+            {
+              label: "LangChain",
+              slug: "workshop/module07",
+            },
+            {
+              label: "Retrieval Augmented Generation (RAG)",
+              slug: "workshop/module08",
+            } 
+          ]
         },
         {
-          label: "Python Development with GitHub Codespaces",
-          slug: "workshop/module03",
-        },
-        {
-          label: "GitHub Models",
-          slug: "workshop/module04",
-        },
-        {
-          label: "OpenAI SDK",
-          slug: "workshop/module05",
-        },
-        {
-          label: "Interactive Development with Jupyter Notebook",
-          slug: "workshop/module06",
-        },
-        {
-          label: "LangChain",
-          slug: "workshop/module07",
-        },
-        {
-          label: "Retrieval Augmented Generation (RAG)",
-          slug: "workshop/module08",
+          label: "Beta Version (in progress)",
+          items: [
+            {
+              label: "Onboarding",
+              slug: "curriculum/onboarding",
+            },
+            {
+              label: "Python Development",
+              slug: "curriculum/python_development",
+            },
+            {
+              label: "GitHub Models",
+              slug: "curriculum/github_models",
+            },
+            {
+              label: "OpenAI SDK",
+              slug: "curriculum/openai_sdk",
+            },
+          ]
         }
       ],
     }),
